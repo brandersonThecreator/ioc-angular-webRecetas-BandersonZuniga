114aa309210f33eceb02f582d49571a87ca8fb1c
Branderson <brandersonr@gmail.com>
2026-03-10 18:04:38 +0100

    pagina web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'ioc-angular-webRecetas-brandersonZuniga';
+  title = "ioc-angular-webRecetas-brandersonZuniga";
+  constructor() {
+    console.log("hola disfruta de todas comidas");
+  }
 }
